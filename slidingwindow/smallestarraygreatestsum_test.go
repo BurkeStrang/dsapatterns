285047284ee77ec8584a030abd34617fa6970bea
Burke Strang <Burke.Strang@vertigis.com>
2025-09-12 14:14:08 -0400

    wip

--- slidingwindow/smallestarraygreatestsum_test.go
+++ slidingwindow/smallestarraygreatestsum_test.go
@@ -4,22 +4,42 @@ import "testing"
 
 func Test_findMinSubArray(t *testing.T) {
 	tests := []struct {
-		name string // description of this test case
-		// Named input parameters for target function.
+		name string
 		S    int
 		arr  []int
 		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Example 1",
+			S:    7,
+			arr:  []int{2, 1, 5, 2, 3, 2},
+			want: 2,
+		},
+		{
+			name: "Example 2",
+			S:    7,
+			arr:  []int{2, 1, 5, 2, 8},
+			want: 1,
+		},
+		{
+			name: "Example 3",
+			S:    8,
+			arr:  []int{3, 4, 1, 1, 6},
+			want: 3,
+		},
+		{
+			name: "No subarray meets S",
+			S:    20,
+			arr:  []int{1, 2, 3, 4},
+			want: 0,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := findMinSubArray(tt.S, tt.arr)
-			// TODO: update the condition below to compare got with tt.want.
-			if true {
+			if got != tt.want {
 				t.Errorf("findMinSubArray() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
-
