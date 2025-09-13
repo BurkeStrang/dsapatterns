cb5cfedd694cb82adef475511ee1c23221effd7b
Burke Strang <burke.strang@outlook.com>
2025-09-12 21:20:04 -0400

    wip

---
+++ slidingwindow/smallestwindowsubstring.go
@@ -0,0 +1,35 @@
+package slidingwindow
+
+// Given a string and a pattern, find the smallest substring in the given string which has all the character occurrences of the given pattern.
+//
+// Example 1:
+// Input: String="aabdec", Pattern="abc"
+// Output: "abdec"
+// Explanation: The smallest substring having all characters of the pattern is "abdec"
+//
+// Example 2:
+//
+// Input: String="aabdec", Pattern="abac"
+// Output: "aabdec"
+// Explanation: The smallest substring having all characters occurrences of the pattern is "aabdec"
+// Example 3:
+//
+// Input: String="abdbca", Pattern="abc"
+// Output: "bca"
+// Explanation: The smallest substring having all characters of the pattern is "bca".
+// Example 4:
+//
+// Input: String="adcad", Pattern="abc"
+// Output: ""
+// Explanation: No substring in the given string has all characters of the pattern
+// Constraints:
+//
+// m == String.length
+// n == Pattern.length
+// 1 <= m, n <= 105
+// String and Pattern consist of uppercase and lowercase English letters.
+
+func findSubstring(str string, pattern string) string {
+	// TODO: Write your code here
+	return ""
+}
