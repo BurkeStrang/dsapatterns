72b1d8b5d47319a1c7fc4cde0ea95997c81182c7
Burke Strang <Burke.Strang@vertigis.com>
2026-03-09 09:13:30 -0400

    wip

--- modifiedbinarysearch/ceiling.go
+++ modifiedbinarysearch/ceiling.go
@@ -1,24 +1,29 @@
 package modifiedbinarysearch
 
+// Example 1:
+// input: [4, 6, 10], key = 5
+// output: 1
 func searchCeilingOfANumber(arr []int, key int) int {
 	if key > arr[len(arr)-1] { // if the 'key' is bigger than the biggest element
 		return -1
 	}
 
-	start, end := 0, len(arr)-1
-	for start <= end {
-		mid := start + (end-start)/2
+	left := 0
+	right := len(arr) - 1
+
+	for left <= right {
+		mid := left + (right-left)/2
 		switch {
 		case key < arr[mid]:
-			end = mid - 1
+			right = mid - 1
 		case key > arr[mid]:
-			start = mid + 1
+			left = mid + 1
 		default: // found the key
 			return mid
 		}
 	}
-	// since the loop is running until 'start <= end', so at the end of the while loop,
-	// 'start == end+1' we are not able to find the element in the given array, so the
-	// next big number will be arr[start]
-	return start
+	// since the loop is running until 'left <= right', so at the right of the while loop,
+	// 'left == right+1' we are not able to find the element in the given array, so the
+	// next big number will be arr[left]
+	return left
 }
