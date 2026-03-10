c3069e8ebaac0a57209210e079fe091d3c7347f3
Burke Strang <Burke.Strang@vertigis.com>
2026-03-10 13:01:09 -0400

    wip

--- bitwisexor/singlenumber.go
+++ bitwisexor/singlenumber.go
@@ -2,7 +2,9 @@ package bitwisexor
 
 // In a non-empty array of integers,
 // every number appears twice except for one,
-// find that single number.
+// find that single number. Use 0(1) space
+// and O(n) time complexity. You can
+// modify the input array.
 //
 // Example 1:
 // Input: 1, 4, 2, 1, 3, 2, 3
@@ -13,9 +15,9 @@ package bitwisexor
 // Output: 9
 
 func findSingleNumber(arr []int) int {
-    num := 0 // Initialize a variable to store the result
-    for i := range arr {
-        num ^= arr[i] // XOR operation to find the single number
-    }
-    return num
+	num := 0 // Initialize a variable to store the result
+	for i := range arr {
+		num ^= arr[i] // XOR operation to find the single number
+	}
+	return num
 }
