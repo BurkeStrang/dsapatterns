a38a365732c91e5b4a7625d40454c6db95b128e8
Burke Strang <Burke.Strang@vertigis.com>
2026-03-10 09:10:00 -0400

    wip

--- modifiedbinarysearch/bitonicarraymax.go
+++ modifiedbinarysearch/bitonicarraymax.go
@@ -24,6 +24,7 @@ package modifiedbinarysearch
 func findMaxInBitonicArray(arr []int) int {
 	left := 0
 	right := len(arr) - 1
+
 	for left < right {
 		mid := left + (right-left)/2
 		if arr[mid] > arr[mid+1] {
