5619be613144a60452839dba730d3042256ef5b5
Burke Strang <Burke.Strang@vertigis.com>
2026-06-09 07:06:28 -0400

    wip

---
+++ backtracking/wordsearch_test.go
@@ -0,0 +1,25 @@
+package backtracking
+
+import "testing"
+
+func Test_exist(t *testing.T) {
+	tests := []struct {
+		name string
+		board [][]byte
+		word  string
+		want  bool
+	}{
+		{name: "test1", board: [][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, word: "ABCCED", want: true},
+		{name: "test2", board: [][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, word: "SEE", want: true},
+		{name: "test3", board: [][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, word: "XYZ", want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := exist(tt.board, tt.word)
+			if got != tt.want {
+				t.Errorf("exist() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
